a35525a60af38d5fd3eb46014d6fc017e19b3df1
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-27 11:32:37 +0500

    updated

--- atm.js
+++ atm.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 let myBalance = 60000; // USD
 let pinCode = 4545;
@@ -9,8 +10,30 @@ let myAnswer = await inquirer.prompt([
     },
 ]);
 if (myAnswer.yourpin === pinCode) {
-    console.log("Your pin code is correct");
+    console.log("Your pin code is correct!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "Please select anyone option",
+            choices: ["Withdrawl", "Check your Balance"],
+        },
+    ]);
+    if (operationAns.operation === "Withdrawl") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Please enter your amount",
+                type: "number",
+            },
+        ]);
+        myBalance -= amountAns.amount;
+        console.log(`Your Remaining Balance is: ${myBalance}`);
+    }
+    else if (operationAns.operation === "Check your Balance") {
+        console.log(`your balance is: ${myBalance}`);
+    }
 }
 else {
-    console.log("Your pin code is incorrect");
+    console.log("Your Pin code is in Correct! Please re-enter your password");
 }
--- atm.ts
+++ atm.ts
@@ -1,5 +1,6 @@
-#! /usr/env/node 
+#! /usr/bin/env node
 import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
 
 let myBalance = 60000; // USD
 let pinCode = 4545;
@@ -11,5 +12,34 @@ let myAnswer = await inquirer.prompt([
     message: "Please enter your pin code",
   },
 ]);
-if(myAnswer.yourpin === pinCode){console.log("Your pin code is correct");}
-else {console.log("Your pin code is incorrect");}
+
+if (myAnswer.yourpin === pinCode) {
+  console.log("Your pin code is correct!!");
+
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      type: "list",
+      message: "Please select anyone option",
+      choices: ["Withdrawl", "Check your Balance"],
+    },
+  ]);
+
+  if (operationAns.operation === "Withdrawl") {
+    let amountAns = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "Please enter your amount",
+        type: "number",
+      },
+    ]);
+
+    myBalance -= amountAns.amount;
+
+    console.log(`Your Remaining Balance is: ${myBalance}`);
+  } else if (operationAns.operation === "Check your Balance") {
+    console.log(`your balance is: ${myBalance}`);
+  }
+} else {
+  console.log("Your Pin code is in Correct! Please re-enter your password");
+}
